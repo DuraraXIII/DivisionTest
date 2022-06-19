f36b9f8948194c51920b36208e957cbf89788e01
DuraraXIII <79815353+DuraraXIII@users.noreply.github.com>
2022-06-19 19:29:43 +1200

    SoleTrader

---
+++ src/main/java/com/practice/test/CalculatorTaxResponse.java
@@ -0,0 +1,15 @@
+package com.practice.test;
+
+public class CalculatorTaxResponse {
+
+    public float getTaxToPay() {
+        return taxToPay;
+    }
+
+    public void setTaxToPay(float taxToPay) {
+        this.taxToPay = taxToPay;
+    }
+
+    private float taxToPay;
+
+}
--- src/main/java/com/practice/test/IncomeTaxCalculatorService.java
+++ src/main/java/com/practice/test/IncomeTaxCalculatorService.java
@@ -3,7 +3,7 @@ package com.practice.test;
 public interface IncomeTaxCalculatorService {
 
     float incomeTaxCalculation(float income);
-
+    CalculatorTaxResponse incomeTaxCalculation(CalculatorTaxRequest request);
 
 
 
